ab7e58904c497e312c16ed14a032e5f0845338cd
Joseph Ghanimah <jfghanimah@gmail.com>
2022-12-28 15:30:06 -0600

    did a lot of stuff

--- data_api.py
+++ data_api.py
@@ -2,7 +2,7 @@ import json
 from riotwatcher import LolWatcher, ApiError
 
 
-lol_watcher = LolWatcher("RGAPI-9cd9f7ab-9125-4641-8820-990d1aa517d7")
+lol_watcher = LolWatcher("RGAPI-1b552721-0ae9-459f-8d83-6f48278d58a6")
 my_region = 'na1'
 
 
@@ -15,7 +15,7 @@ def open_matches():
     with open('save_data/match_info.json') as f:
         match_info = json.loads(f.read())
 
-    for i, match_id in enumerate(match_ids):
+    for i, match_id in enumerate(match_ids.copy()):
         print(f"Geting match data #{i} for: {match_id}")
         match = lol_watcher.match.by_id(my_region, match_id)
 
@@ -35,90 +35,111 @@ def open_matches():
 
         if i % 100 == 0:
             print("saving data")
+            # To my knowledge there cant be dupes because matchid returns 1 info
             # Save every 100 incase of error
             with open('save_data/match_ids.json', 'w') as f:
-                f.write(json.dumps(match_ids))
+                f.write(json.dumps(match_ids)) # used up some match_ids
             with open('save_data/match_ids_checked.json', 'w') as f:
-                f.write(json.dumps(match_ids_checked))
+                f.write(json.dumps(match_ids_checked)) # mark down which ones we used
             with open('save_data/match_info.json', 'w') as f:
-                f.write(json.dumps(match_info))
+                f.write(json.dumps(match_info)) # new match_info! :D
 
 
-
-
+# This function is broken please dont use
 def get_more_match_ids():
-    # Open saved information
+    print("Opening saved data ...")
     with open('save_data/match_ids.json') as f:
         match_ids = json.loads(f.read())
     with open('save_data/puuids.json') as f:
         puuids = json.loads(f.read())
     with open('save_data/puuids_checked.json') as f:
         puuids_checked = json.loads(f.read())
-
+    with open('save_data/match_ids_checked.json') as f:
+        match_ids_checked = json.loads(f.read())
 
     # Get match history for each puuid 
     print(f"Geting match history for each puuid, count:{len(puuids)}")
-    for i, puuid in enumerate(puuids):
-        print(f"getting match history #{i} for {puuid}")
-        player_match_ids = lol_watcher.match.matchlist_by_puuid(my_region, puuid, queue=420, type="ranked", count=100)
-        match_ids += player_match_ids
+    new_match_ids = []
+    for i, puuid in enumerate(puuids.copy()): #1000 puuids =~ 100000 match_ids
+        print(f"#{i} Getting match history from: {puuid}")
+        players_matches = lol_watcher.match.matchlist_by_puuid(my_region, puuid, queue=420, type="ranked", count=100)
+        new_match_ids += players_matches
 
-    match_ids = list(set(match_ids)) # Delete Dupes
+        # Checked puuid so move it to checked and remove from original
+        puuids_checked.append(puuid)
+        del puuids[i]
 
-    # all puuids have been checked
-    puuids_checked += puuids
-    puuids_checked = list(set(puuids_checked)) # Delete Dupes
+        if i+1 % 100 == 0:
+            print("Saving progress... ")
+            seen_before = 0
+            new_match_ids = list(set(new_match_ids)) # Delete Dupes
 
-    # Save variables back to json
-    with open('save_data/puuids.json', 'w') as f:
-        f.write(json.dumps(puuids))
-    with open('save_data/puuids_checked.json', 'w') as f:
-        f.write(json.dumps(puuids_checked))
-    with open('save_data/match_ids.json', 'w') as f:
-        f.write(json.dumps(match_ids))
+            # only append if not already checked
+            for new_id in new_match_ids:
+                if new_id in match_ids_checked: # These matches have been opened
+                    seen_before += 1
+                else:
+                    match_ids.append(new_id)
 
+            print(f"We've already checked {seen_before} of the new match_ids")
+            new_match_ids = [] # reset for next batch of 100
 
+            # Save process
+            match_ids = list(set(match_ids)) # Delete Dupes before
+            puuids_checked = list(set(puuids_checked)) # Delete Dupes
+            # Save variables back to json
+            with open('save_data/puuids.json', 'w') as f:
+                f.write(json.dumps(puuids)) # used up some puuids
+            with open('save_data/puuids_checked.json', 'w') as f:
+                f.write(json.dumps(puuids_checked)) # mark down which ones we used
+            with open('save_data/match_ids.json', 'w') as f:
+                f.write(json.dumps(match_ids)) # new match_ids
+            print("Done saving!")
 
 
 def get_more_puuids():
-    # Open saved information
+    print("Opening saved data ...")
     with open('save_data/match_ids.json') as f:
         match_ids = json.loads(f.read())
     with open('save_data/puuids.json') as f:
         puuids = json.loads(f.read())
     with open('save_data/puuids_checked.json') as f:
-        puuids_checked = json.loads(f.read())
-
+        puuids_checked = json.loads(f.read()) # checked means we have its match_ids
 
     # Gather more puuids
-    print(f"Gather more puuids")
+    print(f"Gathering more puuids:")
     new_puuids = []
-    for match_id in match_ids[0:5]:
-        print(f"Getting participants from: {match_id}")
+    for i, match_id in enumerate(match_ids):
+        print(f"#{i} Getting participants from: {match_id}")
         match = lol_watcher.match.by_id(my_region, match_id)
         new_puuids += match['metadata']['participants']
 
-    new_puuids = list(set(new_puuids)) # Delete dupes
+        if i+1 % 100 == 0:
+            print("Saving progress... ")
+            seen_before = 0
+            new_puuids = list(set(new_puuids)) # Delete dupes
+            # only append if not already checked
+            for new_puuid in new_puuids:
+                if new_puuid in puuids_checked: # We got the matches from these
+                    seen_before += 1
+                else:
+                    puuids.append(new_puuid)
+            
+            print(f"We've already checked {seen_before} of the new puuids")
+            new_puuids = [] # reset for next batch of 100
 
-    puuids_checked = set(puuids_checked)
+            # Save process
+            puuids = list(set(puuids)) # Delete Dupes
+            # Save new_puuids back to json
+            puuids = list(set(puuids)) # Delete dupes
+            with open('save_data/puuids.json', 'w') as f:
+                f.write(json.dumps(puuids)) # new puuids is all there is to change
+            print("Done saving!")
 
-    for puuid in new_puuids:
-        if puuid in puuids_checked:
-            print(f"{puuid:10} already been checked")
-        else:
-            puuids.append(puuid)
-
-    puuids_checked = list(puuids_checked)
-
-    # Save variables back to json
-    with open('save_data/puuids.json', 'w') as f:
-        f.write(json.dumps(puuids))
-    with open('save_data/puuids_checked.json', 'w') as f:
-        f.write(json.dumps(puuids_checked))
-    with open('save_data/match_ids.json', 'w') as f:
-        f.write(json.dumps(match_ids))
+    print("Done getting more puuids!")
 
 
-
-open_matches()
-
+if __name__ == "__main__":
+    #get_more_puuids()
+    #get_more_match_ids()
+    open_matches()
--- features_model.py
+++ features_model.py
@@ -1,27 +1,19 @@
 import pandas as pd
 import tensorflow as tf
+from bake_features import convert_champions
 
 # create one-hot for chess elo leading to wins, add random noise to test how hard it is to dig through noise
 # elo-higher column
 # test win every time and simulate elo higher = 75% chance to win.
 
-# match = [True, 'Ornn', 'Vi', 'Orianna', 'Kaisa', 'Nautilus', 'Fiora', 'LeeSin', 'Zed', 'Caitlyn', 'Senna']
-sample_win_game = [[0.0, 0.33, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
-                    0.66, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
-                    0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-                    0.66, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0,
-                    0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0,
-                    1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                    0.66, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
-                    1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
-                    1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0,
-                    1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0]]
+sample_match = [True, 'Ornn', 'Vi', 'Orianna', 'Kaisa', 'Nautilus', 'Fiora', 'LeeSin', 'Zed', 'Caitlyn', 'Senna']
+sample_data = [convert_champions(sample_match)[1:]]
 
 #Getting csv in df
 df = pd.read_csv('save_data/match_df2.csv')
 
-df_train = df.iloc[:40000, :]
-df_test = df.iloc[40000:, :]
+df_train = df.iloc[:70000, :] # now ive got 79k
+df_test = df.iloc[70000:, :]
 
 features = df_train.copy()
 labels = features.pop('0') # This is the win column
@@ -31,11 +23,11 @@ test_labels = test_features.pop('0') # This is the win column
 
 
 model = tf.keras.Sequential([
-tf.keras.layers.Input(shape=(110,)),
-tf.keras.layers.Dense(100, activation='relu'),
+tf.keras.layers.Input(shape=(200,)),
+tf.keras.layers.Dense(200, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
 tf.keras.layers.BatchNormalization(),
 tf.keras.layers.Dropout(0.5),
-tf.keras.layers.Dense(50, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.01)),
+tf.keras.layers.Dense(100, activation='relu', kernel_regularizer=tf.keras.regularizers.l2(0.001)),
 tf.keras.layers.BatchNormalization(),
 tf.keras.layers.Dropout(0.5),
 tf.keras.layers.Dense(1, activation='sigmoid')
@@ -45,10 +37,10 @@ model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.00001),
                 loss=tf.keras.losses.BinaryCrossentropy(),
                 metrics=['accuracy'])
 
-model.fit(features, labels, epochs=250, batch_size=5)
+model.fit(features, labels, epochs=50, batch_size=10)
 
 print("Testing model.....")
 results = model.evaluate(test_features, test_labels, batch_size=5)
 
-x = model(tf.convert_to_tensor(sample_win_game))
+x = model(tf.convert_to_tensor(sample_data))
 print(x)
